08ac8a7c1c0ffbfebff83f8e455d41a66fd0c8f8
Arunsaini07 <arunsaini8092@email.com>
2026-08-10 16:16:09 +0530

    Add internship experience badges

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -216,7 +216,6 @@ const Hero = () => {
             {/* CTA Buttons */}
             <motion.div variants={item} className="flex flex-wrap gap-4">
               <motion.a
-                href="https://drive.google.com/file/d/1TTZyNGN8-lvkIPYlMwcxXWGaNu8EUEc_/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.04 }}
